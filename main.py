930bc0e94a6b41dd880c45b235fa963306c63c8a
Екатерина Александрова <aliensen65@gmail.com>
2025-04-08 09:13:31 +0300

    Сообщение админам о рестарте бота.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import asyncio
+from typing import Optional
+
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -44,7 +47,40 @@ dp.include_router(admin_course_router)
 dp.include_router(admin_broadcast_router)
 
 
+CHAT_ID: Optional[int] = None
+if os.getenv('CHAT_ID'):
+    try:
+        CHAT_ID = int(os.getenv('CHAT_ID'))
+    except ValueError:
+        logging.error("CHAT_ID должен быть числом!")
+
+
+async def send_to_chat(text: str, chat_id: Optional[int] = None):
+    """
+    Функция для отправки сообщений в чат админов
+    :param text: Текст сообщения
+    :param chat_id: ID чата (если None, используется CHAT_ID из переменных окружения)
+    """
+    target_chat_id = chat_id or CHAT_ID
+    if target_chat_id is None:
+        logging.error("Не указан chat_id для отправки сообщения")
+        return
+
+    try:
+        await bot.send_message(chat_id=target_chat_id, text=text)
+    except Exception as e:
+        logging.error(f"Ошибка отправки в чат {target_chat_id}: {e}")
+
+
 async def on_startup(bot):
+
+    """Действия при перезапуске бота"""
+    if CHAT_ID is not None:
+        await send_to_chat(text="🔄 Бот был перезапущен! Для входа в админ-панель "
+                                "введите команду /admin.")
+    else:
+        logging.warning("CHAT_ID не установлен. Уведомление о перезапуске не отправлено.")
+
     run_param = False
     if run_param:
         await drop_db()
